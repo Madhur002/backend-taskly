da4614229d942b85a61382e45418298ca5713607
madhur <mad@gmail.com>
2023-05-06 20:29:34 +0530

    updated cors policy

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://taskly-backend.onrender.com'
+}));
 // AVAILABLE ROUTES
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todosRoutes);
