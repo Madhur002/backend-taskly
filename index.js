fe4176f329af4b1de0a800484e822cf0317d3b36
madhur <mad@gmail.com>
2023-05-06 21:13:02 +0530

    update

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
 import connectToMongo from "./db.js";
 import express from "express";
-import authRoutes from './routes/auth.js';
-import todosRoutes from './routes/todos.js';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.js";
+import todosRoutes from "./routes/todos.js";
+import cors from "cors";
+import dotenv from "dotenv";
 dotenv.config();
 
-
 connectToMongo();
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 app.use(express.json());
 
-app.use(cors({
-  origin: 'https://taskly-backend.onrender.com',
-  methods: ['GET', 'PUT', 'POST', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 
 // AVAILABLE ROUTES
-app.use('/api/auth', authRoutes);
-app.use('/api/todos', todosRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", todosRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World this is me !')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World this is me !");
+});
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+  console.log(`listening on port ${port}`);
 });
